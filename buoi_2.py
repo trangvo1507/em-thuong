c64580f70a7ea83c2a812232878c269f0ff17cd1
trangvo1507 <votrangthiquyen9@gmail.com>
2025-02-11 15:29:26 +0000

    Buoi 2: for while

---
+++ buoi_2.py
@@ -0,0 +1,19 @@
+print("hello")
+danh_sach = [6, 3, 8, 2, 5, 4, 2, 5, 7, 0]
+sln = danh_sach[1]
+for x in danh_sach:
+    if x >= sln:
+        sln=x
+print(sln)
+        
+
+
+ # m gia tri lon nhat trong danh_sach
+
+# range(<gia tri bat dau>,<gia sai ket thuc>,<so buoc>)
+
+# for <hien-tai> in <danh-sach>:
+#     ...
+
+# while <dieu-kien>:
+#     ...
--- main.py
+++
@@ -1,6 +0,0 @@
-a = int(input("so thu nhat "))
-b = int(input("so thu hai "))
-if a % 2 == 0 and b % 2 == 0 or a % 2 == 1 and b % 2 == 1:
-    print("tong hai so la so chan")
-else:
-    print("tong hai so la so le")
