b3c7120c2cf40dcf16d6480f40cb6c93ffe5e1ee
trangvo1507 <votrangthiquyen9@gmail.com>
2025-02-12 14:11:00 +0000

    Buoi 3: practice for while

---
+++ buoi_3.py
@@ -0,0 +1,11 @@
+danh_sach = [6, 3, 8, 2, 5, 4, 2, 5, 7, 0]
+tong = 0
+dst = []
+for x in danh_sach:
+    tong = tong + x
+    dst += [tong]
+print(dst)
+stt_a = 2
+stt_b = 6
+dstd = dst[5] - dst[1]
+print(dstd)
